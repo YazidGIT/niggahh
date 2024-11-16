e54f5f53dd2cf73e43503dd411e7df61743d779a
lelouch106 <britannialelouch782@gmail.com>
2024-11-16 13:12:54 +0800

    Update prefix.js

--- plugins/commands/group/prefix.js
+++ plugins/commands/group/prefix.js
@@ -1,71 +1,33 @@
-const config = {
-    aliases: ["pf", "setprefix", "setpf"],
-    permissions: [1, 2],
-    description: "Set prefix for group",
-    usage: "<prefix>",
-    cooldown: 5,
-    credits: "XaviaTeam",
-};
+import axios from 'axios';
 
 const langData = {
     "en_US": {
-        "prefix.get": "Default prefix: {default}\nCurrent prefix: {current}",
-        "prefix.set": "Prefix has been set to {newPrefix}",
-        "prefix.tooLong": "Prefix must be less than 5 characters",
-        "notGroup": "This command only works in group",
-        "threadDataNotExists": "Thread data not exists",
+        "prefix.get": "𝙇𝙚𝙡𝙤𝙪𝙘𝙝 𝚙𝚛𝚎𝚏𝚒𝚡 𝚒𝚜: [ {prefix} ]"
     },
     "vi_VN": {
-        "prefix.get": "Prefix mặc định: {default}\nPrefix hiện tại: {current}",
-        "prefix.set": "Prefix đã được đặt thành {newPrefix}",
-        "prefix.tooLong": "Prefix phải ít hơn 5 ký tự",
-        "notGroup": "Lệnh này chỉ hoạt động trong nhóm",
-        "threadDataNotExists": "Dữ liệu nhóm không tồn tại",
-    },
-    "ar_VN": {
-        "prefix.get": "بادئة افتراضية: {default}\nالبادئة الحالية: {current}",
-        "prefix.set": "تم تعيين البادئة على {newPrefix}",
-        "prefix.tooLong": "يجب أن تكون البادئة أقل من 5 أحرف",
-        "notGroup": "هذا الأمر يعمل فقط في المجموعة",
-        "threadDataNotExists": "بيانات المجموعة غير موجودة",
-    },
+        "prefix.get": "Prefix hiện tại là: {prefix}"
+    }
 };
 
-/** @type {TOnCallCommand} */
-async function onCall({ message, args, data, getLang, prefix }) {
-    const { isGroup, threadID } = message;
-    const { thread } = data;
+async function onCall({ message, getLang, data }) {
+    if (message.body === "prefix" && message.senderID !== global.botID) {
+        
+        const attachment = await axios.get("https://imgur.com/a/0MPs8pt", { responseType: "stream" })
+            .then(response => response.data)
+            .catch(err => console.error("Error fetching image: ", err));
 
-    if (!isGroup) return message.reply(getLang("notGroup"));
-    if (thread == null) return message.reply(getLang("threadDataNotExists"));
-    if (typeof thread.data != "object") thread.data = {};
-
-    if (!args[0])
-        return message.reply(
-            getLang("prefix.get", {
-                default: global.config.PREFIX,
-                current: prefix,
-            })
-        );
-
-    const newPrefix = args[0];
-    if (newPrefix.length > 5) return message.reply(getLang("prefix.tooLong"));
-
-    await global.controllers.Threads.updateData(threadID, {
-        prefix: newPrefix,
-    });
-    global.api.changeNickname(
-        `[ ${newPrefix} ] ${global.config.NAME || "Xavia"}`,
-        threadID,
-        global.botID,
-        (_) => {
-            message.reply(getLang("prefix.set", { newPrefix }));
-        }
-    );
+        
+        message.reply({
+            body: getLang("prefix.get", {
+                prefix: data?.thread?.data?.prefix || global.config.PREFIX
+            }),
+            attachment: attachment 
+        });
+    }
+    return;
 }
 
 export default {
-    config,
     langData,
-    onCall,
+    onCall
 };
